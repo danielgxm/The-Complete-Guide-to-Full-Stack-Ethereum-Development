25ec73fd48179b89b0dc1c9b3c67cc47979c66ce
daniel <daniel_gao@qq.com>
2022-02-26 14:07:36 +0800

    token

---
+++ react-dapp/contracts/Token.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    //Token名称
+    string public token_name = "DanielGao Token";
+    //Token代码
+    string public token_code = "DGT";
+    //Token总量
+    uint256 public token_total = 1_000_000;
+    //地址余额
+    mapping(address => uint256) balances;
+
+    //构造函数
+    constructor() {
+        balances[msg.sender] = token_total;
+    }
+
+    //发送Token
+    function transfer(address to, uint256 count) external {
+        require(balances[msg.sender] >= count, "Not enough token");
+        balances[msg.sender] -= count;
+        balances[to] += count;
+    }
+
+    //查看地址余额
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
